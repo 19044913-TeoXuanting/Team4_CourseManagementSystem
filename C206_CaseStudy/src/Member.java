3af767c6b10c06a4584217f44595816dfb81ad49
19026991 <19026991@DESKTOP-4UEMC0N>
2020-08-23 21:25:18 +0800

    member class

--- C206_CaseStudy/src/Member.java
+++ C206_CaseStudy/src/Member.java
@@ -1,4 +1,69 @@
 
 public class Member {
+	
+	private String name;
+	private String gender;
+	private int mobile_number;
+	private String email;
+	private int dob;
+	private String residence;
+
+	public Member(String name, String gender, int mobile_number, String email, int dob, 
+			String residence) {
+		this.name = name;
+		this.gender = gender;
+		this.mobile_number = mobile_number;
+		this.email = email;
+		this.dob = dob;
+		this.residence = residence;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getMobile_number() {
+		return mobile_number;
+	}
+
+	public void setMobile_number(int mobile_number) {
+		this.mobile_number = mobile_number;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getDob() {
+		return dob;
+	}
+
+	public void setDob(int dob) {
+		this.dob = dob;
+	}
+
+	public String getResidence() {
+		return residence;
+	}
+
+	public void setResidence(String residence) {
+		this.residence = residence;
+	}
 
 }
